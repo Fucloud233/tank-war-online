2ed36e9ecbc5702e472978ca8bba3dc560b0416d
Fucloud <270928474@qq.com>
2023-05-29 17:02:35 +0800

    添加 枚举类型INVALID

--- app/src/main/java/com/tankWar/game/entity/Direction.java
+++ app/src/main/java/com/tankWar/game/entity/Direction.java
@@ -1,5 +1,20 @@
 package com.tankWar.game.entity;
 
 public enum Direction {
-    CENTER, LEFT, UP, RIGHT, DOWN;
+    INVALID(0),
+    CENTER(0x1),
+    LEFT(0x10),
+    UP(0x100),
+    RIGHT(0x1000),
+    DOWN(0x10000);
+
+    int value = 0;
+
+    public int getValue() {
+        return value;
+    }
+
+    Direction(int value) {
+        this.value = value;
+    }
 }
