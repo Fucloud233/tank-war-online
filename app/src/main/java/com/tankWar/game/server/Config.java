e6ddb6a833f69fa4b9ae57ddce9a68f059fef342
Fucloud <270928474@qq.com>
2023-05-29 14:52:18 +0800

    更新 配置文件

--- app/src/main/java/com/tankWar/game/server/Config.java
+++ app/src/main/java/com/tankWar/game/server/Config.java
@@ -1,6 +1,6 @@
 package com.tankWar.game.server;
 
 public class Config {
-    public final static String ip = "172.27.86.194";
+    public final static String ip = "127.0.0.1";
     public final static int port = 20000;
 }
