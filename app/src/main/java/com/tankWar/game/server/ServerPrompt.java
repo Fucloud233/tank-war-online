05cfb1cda2ed3eb89a4dff6cea9933562cbdb288
Fucloud <270928474@qq.com>
2023-06-01 11:03:21 +0800

    上传 服务端的提示信息

---
+++ app/src/main/java/com/tankWar/game/server/ServerPrompt.java
@@ -0,0 +1,41 @@
+package com.tankWar.game.server;
+
+public enum ServerPrompt {
+
+    // Prompt
+    AllSuccess("全部成功", 0),
+    AllConnected("客户端全部连接成功", 0),
+    AllReceived("客户端消息全部接收成功", 0),
+    AllSent("客户端消息全部发送成功", 0),
+
+    // Error
+    ReceiveFail("接收来自客户端的消息失败", 2),
+    SendFail("向客户端发送消息失败", 2);
+
+
+
+    String text;
+    // 0 消息 1 警告 2 错误
+    int type;
+
+    ServerPrompt(String text, int type) {
+        this.text = text;
+        this.type = type;
+    }
+
+    public void print() {
+        String typeText = null;
+
+        switch (type) {
+            case 0->{typeText = "[Info] ";}
+            case 1->{typeText = "[Warn] ";}
+            case 2->{typeText = "[Error] ";}
+        }
+
+        System.out.println(typeText + text);
+    }
+
+
+}
+
+
