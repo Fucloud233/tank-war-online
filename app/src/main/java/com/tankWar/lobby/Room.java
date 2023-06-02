48883e59ef78b1dfb15616886049a890ff40cfd7
bbyyj <132241269+bbyyj@users.noreply.github.com>
2023-06-02 16:26:16 +0800

    Update Room.java

--- app/src/main/java/com/tankWar/lobby/Room.java
+++ app/src/main/java/com/tankWar/lobby/Room.java
@@ -2,7 +2,6 @@ package com.tankWar.lobby;
 
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Vector;
 
 public class Room {
@@ -12,10 +11,8 @@ public class Room {
     private int enter_num=0;
     private String password;
     private ArrayList<String> players = new ArrayList<>();
-    /////////////////////////
     private static Vector onlineUser = new Vector(10, 5);//保存在线用户的用户名
     private static Vector socketUser = new Vector(10, 5);//保存在线用户的Socket对象
-    ///////////////////////////
 
     public Room(String roomNum,int userNum,String passw){
         RoomNum=roomNum;
@@ -23,79 +20,74 @@ public class Room {
         password=passw;
 
     }
+    //设置房间的使用状态
     public void setIs_used(boolean a){
         is_used=a;
     }
+    //设置房间的人数
     public void setUser_num(int a){
         user_num=a;
 
     }
+    //设置房间的密码
     public void setPassword(String a){
         password=a;
 
     }
+    //添加房间里的玩家账号，好像和下面的函数重复了，你可以改改，sorry~
     public void addPlayer(String a){
         players.add(a);
         enter_num++;
 
     }
-    ////////////////////////////////////
+    //添加房间里的玩家账号
     public void addOnlineUser(String name){
         onlineUser.addElement(name);
     }
+    //添加房间里的玩家套接字，注意因为这个函数和上面那个函数往往都是同步添加的，所以它们的下标一样的话，对应的是一个用户
+    //可以再添加的时候合在一起加
     public void addSocketUser(Socket s){
         socketUser.addElement(s);
     }
+    //移除房间里的玩家账号
     public void removeOnlineUser(int i){
+        enter_num-=1;
         onlineUser.removeElementAt(i);
     }
+    //移除房间里的玩家套接字，感觉这个可以和上面的函数合在一起操作
     public void removeSocketUser(int i){
         socketUser.removeElementAt(i);
     }
+    //根据下标找到玩家账号
     public String findOnlineUser(int i){
         return (String) onlineUser.elementAt(i);
     }
+    //根据下标找到玩家套接字
     public Socket findSocketUser(int i){
-        enter_num-=1;
+
         return (Socket) socketUser.elementAt(i);
     }
+    //返回房间人数，好像和getEnter_num重复了
     public int sizeOfOnlineUser(){
         return onlineUser.size();
     }
-    /////////////////////////////////////
-    public ArrayList<String> getPlayers(){
-        return players;
-    }
-
+    //返回该房间的房间号，我是直接用房主的账号做房间号的
     public String getRoomNum(){return RoomNum;}
-
+    //返回进入房间的人数
     public int getEnter_num() {
         return enter_num;
     }
-
+    //返回房间里设置的人数
     public int getUser_num() {
         return user_num;
     }
+    //返回房间的密码
     public String getPassword(){return password;}
-
+    //返回房间是否被使用的状态
     public boolean isIs_used() {
         return is_used;
     }
-    public boolean compareWithPassword(String a){
-        if (Objects.equals(password, a)){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    ////////////////////////////////
-    public boolean compareUserNum(){
-        if (enter_num<user_num){
-            return true;
-        }
-        return false;
-    }
+    //查看房间内是否已经有用户的账号，有的话就不重复添加了
     public boolean findUserId(String userId) {
         int i = players.indexOf(userId);
         if (i == -1) {
@@ -103,5 +95,5 @@ public class Room {
         }
         return true;
     }
-    /////////////////////////////////
+
 }
