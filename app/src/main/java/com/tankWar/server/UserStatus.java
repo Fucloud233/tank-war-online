3ffcbfad06592e092a10ca870e9376598d5ffe9e
Fucloud <270928474@qq.com>
2023-06-14 18:22:49 +0800

    添加 用于状态判断的函数

--- app/src/main/java/com/tankWar/server/UserStatus.java
+++ app/src/main/java/com/tankWar/server/UserStatus.java
@@ -1,5 +1,24 @@
 package com.tankWar.server;
 
 public enum UserStatus {
-    Null, NoReady, Ready, Playing
+    Null, NoReady, Ready, Playing;
+
+//    Null(0), NoReady(1), Ready(2), Playing(3);
+
+//    int value;
+//    UserStatus(int value) {
+//        this.value = value;
+//    }
+
+    public boolean isInLobby() {
+        return this == Null;
+    }
+
+    public boolean isInRoom() {
+        return this == NoReady || this == Ready;
+    }
+
+    public boolean isPlaying() {
+        return this == Playing;
+    }
 }
