41e820c66c942c40bbd6f09ac0bc7e1072fc704e
Fucloud <270928474@qq.com>
2023-06-14 18:21:10 +0800

    重构 Room, 使其包括Game对象(用于处理游戏相关的接收和发送)

---
+++ app/src/main/java/com/tankWar/server/Game.java
@@ -0,0 +1,117 @@
+package com.tankWar.server;
+
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class Game {
+    // 所有玩家
+//    HashMap<SocketChannel, User> users = new HashMap<>();
+    Vector<SocketChannel> sockets;
+    Vector<Integer> restPlayer;
+
+
+    int totalGameNum = 2, curGameNum = 0;
+    // 记录玩家数量
+    // 考虑有人会退出游戏, 保持id的一致性
+//    Vector<Integer> totalPlayer;
+
+    // 记录分数
+    int[] scores;
+
+    public Game(HashMap<SocketChannel, User> users) {
+        int size = users.size();
+
+        // 初始化数组
+        sockets = new Vector<>(size);
+        restPlayer = new Vector<>(size);
+        scores = new int[size];
+
+        int i = 0;
+        for(Map.Entry<SocketChannel, User> e: users.entrySet()) {
+            sockets.addElement(e.getKey());
+            restPlayer.addElement(i);
+        }
+    }
+
+    void resetRestPlayer() {
+        restPlayer.clear();
+
+        for(int i=0; i<sockets.size(); i++) {
+            restPlayer.addElement(i);
+        }
+    }
+
+    // 记录死亡 并当只剩下一个人的时候返回true
+    public DeadStatus dead(int id) {
+        // 1. 记录死亡消息
+        restPlayer.removeElement(id);
+
+        if(restPlayer.size() != 1)
+            return DeadStatus.Null;
+
+        // 2. 当剩下移位玩家则重置游戏
+        int winnerId = restPlayer.get(0);
+
+        // 重置死亡信息
+        resetRestPlayer();
+
+        // 3. 加分
+        scores[winnerId]++ ;
+
+        // 4. 判断是否是最后场游戏
+        if(curGameNum < totalGameNum) {
+            curGameNum++;
+            return DeadStatus.GameOver;
+        }
+
+        return DeadStatus.MatchOver;
+    }
+
+    public int getTotalPlayerNum() {
+        return sockets.size();
+    }
+
+    public int getRestPlayerNum() {
+        return restPlayer.size();
+    }
+
+    // 得到总局数
+    public int getTotalGameNum() {
+        return totalGameNum;
+    }
+
+    // 得到总局数
+    public int getCurGameNum() {
+        return curGameNum;
+    }
+
+    // 得到分数
+    public int[] getScores() {
+        return scores;
+    }
+
+    // 得到所有玩家
+    public Vector<SocketChannel> getAllSockets() {
+        return sockets;
+    }
+}
+
+enum DeadStatus{
+    // 无事发生
+    Null,
+    // 一场游戏结束
+    GameOver,
+    // 所有常数结束
+    MatchOver;
+
+    public boolean shouldReset() {
+        return this == GameOver;
+    }
+
+    public boolean shouldOver() {
+        return this == MatchOver;
+    }
+
+};
--- app/src/main/java/com/tankWar/server/Room.java
+++ app/src/main/java/com/tankWar/server/Room.java
@@ -1,30 +1,29 @@
 package com.tankWar.server;
 
-import java.net.Socket;
-import java.util.Vector;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import javafx.util.Pair;
 
 public class Room {
     // Room信息 (房号使用房主账号标记)
-    String roomNum;
-    String roomName;
+    String roomNum, roomName, password;
     int maxUserNum;
-    String password;
     boolean havePassword;
 
     // Room状态
-    private boolean status = false;
+    boolean status = false;
+    Game game = null;
 
-    // 房主信息
-    int curUserNum = 0; //房间的当前人数
-    Vector<UserInfo> users;
+    // 所有用户
+    HashMap<SocketChannel, User> users = new HashMap<>();
 
-    public Room(String roomNum, String roomName, int userNum){
+
+    public Room(String roomNum, String roomName, int userNum)  {
         this.roomNum = roomNum;
         this.roomName = roomName;
         this.maxUserNum = userNum;
         this.havePassword = false;
-
-        users = new Vector<>(userNum);
     }
 
     public Room(String roomNum, String roomName, int userNum, String password){
@@ -35,84 +34,55 @@ public class Room {
 
     //清空房间内的所有内容
     public void cleanAll(){
-        curUserNum=0;
+        // 修改玩家状态
+        for(User user: users.values()) {
+            user.setStatus(UserStatus.Null);
+            user.leaveRoom();
+        }
+
+        // 清除房间的所用用户
+        users.clear();
     }
 
     // 添加房间里的玩家昵称、账号、套接字和状态。初始状态是wait
-    public void addOnlineUser(UserInfo user){
+    public void addOnlineUser(SocketChannel socket, User user){
+        user.setStatus(UserStatus.NoReady);
+        user.joinRoom(this);
         // 第一个玩家就是房主
-        users.add(user);
-        curUserNum += 1;
+        users.put(socket, user);
     }
 
     //通过索引移除房间里的玩家
-    public void removeOnlineUser(int i){
-        users.removeElementAt(i);
-        curUserNum -= 1;
-    }
-
-    // 根据下标找到玩家状态
-    public String getUserStatus(int i){
-//        System.out.println("[info] "+statusUser.elementAt(i));
-        String status = "";
-
-        switch (users.get(i).isStatus()){
-            case Ready -> status = "已准备";
-            case NoReady -> status = "未准备";
+    public void removeOnlineUser(SocketChannel socket){
+        // 如果玩家不存在则直接返回
+        if(!users.containsKey(socket)) {
+            return;
         }
-
-        return status;
+        User user = users.get(socket);
+        user.setStatus(UserStatus.Null);
+        user.leaveRoom();
+        // 删除玩家
+        users.remove(socket);
     }
 
-    // 切换对应下表的玩家的状态
-    public void changeUserStatus(int index, UserStatus status) {
-        UserInfo user = users.get(index);
-        user.setStatus(status);
-//        System.out.println("[info] 修改后"+statusUser.elementAt(index));
+    //返回进入房间的人数
+    public int getOnlineUserNum() {
+        return users.size();
     }
 
-    public void changeUserStatus(String name, UserStatus status) {
-        for(UserInfo user: users) {
-            if(user.getNickName().equals(name)) {
-                user.setStatus(status);
-                return;
-            }
-        }
-
-        System.out.println("[Error]: 该用户不存在");
-//        System.out.println("[info] 修改后"+statusUser.elementAt(index));
-    }
-
-    // 根据账号找到玩家的索引
-//    public int getAccountIndex(String account){
-//        return onlineUser.indexOf(account);
-//    }
-
-    //根据下标找到玩家昵称
-    public String findNameUser(int i){
-        return users.get(i).getNickName();
-    }
-
-    //根据下标找到玩家账号
-    public String findOnlineUser(int i){
-        return users.get(i).getAccount();
-    }
-
-    //根据下标找到玩家套接字
-    public Socket findSocketUser(int i){
-        return users.get(i).getSocket();
-    }
-
-    // 获得所有用户信息
-    public Vector<UserInfo> getAllUsers() {
+    public HashMap<SocketChannel, User> getAllUsers() {
         return users;
     }
 
-    public Socket[] getAllSockets() {
-        Socket[] sockets = new Socket[users.size()];
-        for(int i=0; i<users.size(); i++)
-            sockets[i] = users.get(i).getSocket();
-        return sockets;
+    // 根据用户名查找用户
+    public Pair<SocketChannel, User> getUser(String nickName) {
+        for(Map.Entry<SocketChannel, User> e: users.entrySet()) {
+            if(e.getValue().getNickName().equals(nickName)) {
+                return new Pair<>(e.getKey(), e.getValue());
+            }
+        }
+
+        return null;
     }
 
     public String[] getAllNickNames() {
@@ -135,12 +105,6 @@ public class Room {
         return users.get(0).getNickName();
     }
 
-
-    //返回进入房间的人数
-    public int getCurUserNum() {
-        return users.size();
-    }
-
     //返回房间里设置的人数
     public int getMaxUserNum() {
         return maxUserNum;
@@ -150,38 +114,49 @@ public class Room {
     public boolean havePassword() {return havePassword;}
     //返回房间的密码
     public String getPassword(){return password;}
+    // 设置密码
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     //返回房间的状态
     public boolean getStatus() {
         return status;
     }
-
-    // 改变房间的状态 和 玩家状态
-    public void setStatus(boolean status){
-        status = true;
-        // 设置所有玩家为游戏状态
-        for(UserInfo user: users)
-            user.setStatus(UserStatus.Playing);
-    }
-
 
     // 返回是否满了
     public boolean isFull() {
         return users.size() >= maxUserNum;
     }
 
-    public boolean checkPassword(String password) {
-        return password.equals(this.password);
-    }
-
     public boolean isHost(String name) {
         return users.get(0).getNickName().equals(name);
+    }
+
+    public boolean checkPassword(String password) {
+        return password.equals(this.password);
     }
 
     // 检查是否房间中的用户都是准备好的
     public boolean checkAllUsersReady() {
         boolean flag = true;
-        for (UserInfo user: users)
-            flag &= user.isStatus() == UserStatus.Ready;
+        for (User user: users.values())
+            flag &= user.getStatus() == UserStatus.Ready;
         return flag;
+    }
+
+    // 改变房间的状态 和 玩家状态
+    public void startGame(boolean status){
+        status = true;
+        // 设置所有玩家为游戏状态
+        for(User user: users.values())
+            user.setStatus(UserStatus.Playing);
+
+        // todo 初始化操作
+        game = new Game(this.users);
+    }
+
+    public Game getGame() {
+        return game;
     }
 }
